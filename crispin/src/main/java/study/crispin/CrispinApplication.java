c67432e07bebc7953bf08f7f2469466212312ccd
Shin Hee-Cheol <shoy1415@gmail.com>
2024-02-27 23:59:14 +0900

    :tada: Init: project init by crispin
    - 미니 프로젝트 생성

---
+++ crispin/src/main/java/study/crispin/CrispinApplication.java
@@ -0,0 +1,13 @@
+package study.crispin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrispinApplication.class, args);
+    }
+
+}
