84c91570c5542f00c6803a11372171c9019345e1
leeedohyun <ldh19738@gmail.com>
2024-02-28 14:34:56 +0900

    feat: 도메인 연관관계 정의

--- leeedohyun/src/main/java/inflearn/mini/employee/domain/Employee.java
+++ leeedohyun/src/main/java/inflearn/mini/employee/domain/Employee.java
@@ -4,10 +4,13 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
+import inflearn.mini.team.domain.Team;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +33,9 @@ public class Employee {
     private LocalDate workStartDate;
 
     private LocalDate birthday;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Team team;
 
     public Employee(final String name, final boolean isManager, final LocalDate workStartDate,
                     final LocalDate birthday) {
--- leeedohyun/src/main/java/inflearn/mini/team/domain/Team.java
+++ leeedohyun/src/main/java/inflearn/mini/team/domain/Team.java
@@ -1,11 +1,16 @@
 package inflearn.mini.team.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
+import inflearn.mini.employee.domain.Employee;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +27,9 @@ public class Team {
 
     @Column(name = "team_name")
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Employee> employees = new ArrayList<>();
 
     public Team(final String name) {
         this.name = name;
