385e0631cb6d4a8b9aa558d83b6530eb76b5ec5e
김병조 <belongs2jo@gmail.com>
2024-02-29 12:01:24 +0900

    init : project init

---
+++ jokim/workHub/src/main/java/inflearn/mini/workHub/WorkHubApplication.java
@@ -0,0 +1,13 @@
+package inflearn.mini.workHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHubApplication.class, args);
+	}
+
+}
---
+++ jokim/workHub/src/test/java/inflearn/mini/workHub/WorkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.mini.workHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
