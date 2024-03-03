1b397d360c70883aa137856e770cbea03d6ecfb1
crispin <shoy1415@gmail.com>
2024-03-03 11:31:49 +0900

    :sparkles: Feat: team controller
      - 팀 컨트롤러 추가

--- crispin/src/main/java/study/crispin/team/application/request/TeamRegistrationRequest.java
+++ crispin/src/main/java/study/crispin/team/application/request/TeamRegistrationRequest.java
@@ -1,4 +1,11 @@
 package study.crispin.team.application.request;
 
-public record TeamRegistrationRequest(String name) {
+import jakarta.validation.constraints.NotBlank;
+
+public record TeamRegistrationRequest(
+        @NotBlank(message = "이름은 필수 입력값 입니다.") String name
+) {
+    public static TeamRegistrationRequest of(String name) {
+        return new TeamRegistrationRequest(name);
+    }
 }
