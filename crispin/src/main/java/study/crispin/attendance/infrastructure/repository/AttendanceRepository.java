9274845bf64ed771a490c22d4dd31bd1a08b1d14
crispin <shoy1415@gmail.com>
2024-03-05 23:49:27 +0900

    :sparkles: Feat: add existsByMemberIdAndDateRange method to the attendance repository domain
      - existsByMemberIdAndDateRange 메서드 추가

--- crispin/src/main/java/study/crispin/attendance/infrastructure/repository/AttendanceRepository.java
+++ crispin/src/main/java/study/crispin/attendance/infrastructure/repository/AttendanceRepository.java
@@ -2,10 +2,13 @@ package study.crispin.attendance.infrastructure.repository;
 
 import study.crispin.attendance.domain.Attendance;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface AttendanceRepository {
     Attendance save(Attendance attendance);
 
     Optional<Attendance> findByMemberId(Long memberId);
+
+    boolean existsByMemberIdAndDateRange(Long memberId, LocalDate startDate, LocalDate endDate);
 }
