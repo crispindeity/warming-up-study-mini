473b81418303eb2fa149dce4036911dc85a9f9cf
crispin <shoy1415@gmail.com>
2024-03-05 23:51:05 +0900

    :sparkles: Feat: add already clock in exception
      - 재 출근 등록 방지 예외 로직 추가

--- crispin/src/main/java/study/crispin/attendance/application/AttendanceService.java
+++ crispin/src/main/java/study/crispin/attendance/application/AttendanceService.java
@@ -4,41 +4,42 @@ import study.crispin.attendance.domain.Attendance;
 import study.crispin.attendance.infrastructure.repository.AttendanceRepository;
 import study.crispin.attendance.presentation.response.ClockInResponse;
 import study.crispin.attendance.presentation.response.ClockOutResponse;
-import study.crispin.common.DateTimeHolder;
+import study.crispin.common.exception.ExceptionMessage;
+import study.crispin.common.exception.VerificationException;
 import study.crispin.member.domain.Member;
 import study.crispin.member.infrastructure.repository.MemberRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class AttendanceService {
 
-    private final DateTimeHolder dateTimeHolder;
     private final MemberRepository memberRepository;
     private final AttendanceRepository attendanceRepository;
 
     public AttendanceService(
-            DateTimeHolder dateTimeHolder,
             MemberRepository memberRepository,
             AttendanceRepository attendanceRepository
     ) {
-        this.dateTimeHolder = dateTimeHolder;
         this.memberRepository = memberRepository;
         this.attendanceRepository = attendanceRepository;
     }
 
-    public ClockInResponse clockIn(Long memberId) {
-        LocalDateTime clockInDateTime = dateTimeHolder.now();
+    public ClockInResponse clockIn(Long memberId, LocalDateTime clockInDateTime) {
+        verifyTodayClockIn(memberId, clockInDateTime);
+
         Member findedMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException(""));
+
         Attendance attendance = Attendance.clockIn(findedMember, clockInDateTime);
         Attendance savedAttendance = attendanceRepository.save(attendance);
+
         return ClockInResponse.of(
                 savedAttendance.id(), savedAttendance.member().name(), savedAttendance.clockInDateTime()
         );
     }
 
-    public ClockOutResponse clockOut(Long memberId) {
-        LocalDateTime clockOutDateTime = dateTimeHolder.now().plusHours(9L);
+    public ClockOutResponse clockOut(Long memberId, LocalDateTime clockOutDateTime) {
         Attendance findedAttendance = attendanceRepository.findByMemberId(memberId)
                 .orElseThrow(() -> new IllegalArgumentException(""));
         Attendance clockOutedAttendance = Attendance.clockOut(findedAttendance, clockOutDateTime);
@@ -49,5 +50,14 @@ public class AttendanceService {
                 savedAttendance.clockInDateTime(),
                 savedAttendance.clockOutDateTime()
         );
+    }
+
+    private void verifyTodayClockIn(Long memberId, LocalDateTime clockInDateTime) {
+        LocalDate startDate = clockInDateTime.toLocalDate();
+        LocalDate endDate = startDate.plusDays(1L);
+
+        if (attendanceRepository.existsByMemberIdAndDateRange(memberId, startDate, endDate)) {
+            throw new VerificationException(ExceptionMessage.ALREADY_CLOCKED_IN);
+        }
     }
 }
