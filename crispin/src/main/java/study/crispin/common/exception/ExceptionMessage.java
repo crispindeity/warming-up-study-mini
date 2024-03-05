506b256d5c55d7a4a65976f2fb0bb56e7f7bca10
crispin <shoy1415@gmail.com>
2024-03-05 23:51:56 +0900

    :sparkles: Feat: add exception message
      - 출근 등록 예외 메시지 추가

--- crispin/src/main/java/study/crispin/common/exception/ExceptionMessage.java
+++ crispin/src/main/java/study/crispin/common/exception/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     NOT_MEMBER_OF_TEAM("팀에 소속된 멤버가 아닙니다."),
     ALREADY_MANAGER_REGISTERED("팀에 이미 매니저가 등록되어 있습니다."),
     TEAM_NAME_ALREADY_EXISTS("이미 존재하는 팀 이름 입니다."),
-    NOT_AFFILIATED_ANY_TEAM("소속된 팀이 없습니다.");
+    NOT_AFFILIATED_ANY_TEAM("소속된 팀이 없습니다."),
+    ALREADY_CLOCKED_IN("이미 출근 등록이 되어 있습니다.");
 
     private final String message;
 
