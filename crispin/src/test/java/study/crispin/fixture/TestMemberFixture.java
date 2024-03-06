207ee4c51f9ef20e9d5e7c2420d3ca7fac0be609
crispin <shoy1415@gmail.com>
2024-03-06 22:01:29 +0900

    :white_check_mark: Test: add member test fixture
      - 테스트 용 멤버 생성 추가

--- crispin/src/test/java/study/crispin/fixture/TestMemberFixture.java
+++ crispin/src/test/java/study/crispin/fixture/TestMemberFixture.java
@@ -8,4 +8,8 @@ public class TestMemberFixture {
     public static Member 멤버_생성(String name, String teamName, LocalDate birthday, LocalDate workStartDate) {
         return Member.of(name, teamName, birthday, workStartDate);
     }
+
+    public static Member 멤버_생성(Long id, String name, String teamName, LocalDate birthday, LocalDate workStartDate) {
+        return Member.of(id, name, teamName, birthday, workStartDate);
+    }
 }
