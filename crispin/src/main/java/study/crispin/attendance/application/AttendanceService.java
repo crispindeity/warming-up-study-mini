5463ab6c98cf5591b73884503e981f06f3a4814b
crispin <shoy1415@gmail.com>
2024-03-06 21:59:54 +0900

    :sparkles: Feat: add work hours inquiry service
      - 근무 시간 조회 요청 및 응답 추가

--- crispin/src/main/java/study/crispin/attendance/application/AttendanceService.java
+++ crispin/src/main/java/study/crispin/attendance/application/AttendanceService.java
@@ -1,9 +1,12 @@
 package study.crispin.attendance.application;
 
+import study.crispin.attendance.application.request.WorkHoursInquiryRequest;
 import study.crispin.attendance.domain.Attendance;
 import study.crispin.attendance.infrastructure.repository.AttendanceRepository;
 import study.crispin.attendance.presentation.response.ClockInResponse;
 import study.crispin.attendance.presentation.response.ClockOutResponse;
+import study.crispin.attendance.presentation.response.WorkHoursInquiryResponse;
+import study.crispin.attendance.presentation.response.WorkHoursInquiryResponses;
 import study.crispin.common.LocalDateUtil;
 import study.crispin.common.exception.NotFoundException;
 import study.crispin.common.exception.VerificationException;
@@ -12,6 +15,7 @@ import study.crispin.member.infrastructure.repository.MemberRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static study.crispin.common.exception.ExceptionMessage.*;
 
@@ -31,8 +35,7 @@ public class AttendanceService {
     public ClockInResponse clockIn(Long memberId, LocalDateTime clockInDateTime) {
         verifyTodayClockIn(memberId, clockInDateTime);
 
-        Member findedMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NotFoundException(UNREGISTERED_MEMBER));
+        Member findedMember = findMemberById(memberId);
 
         Attendance attendance = Attendance.clockIn(findedMember, clockInDateTime);
         Attendance savedAttendance = attendanceRepository.save(attendance);
@@ -60,6 +63,18 @@ public class AttendanceService {
         );
     }
 
+    public WorkHoursInquiryResponses workHoursInquiry(WorkHoursInquiryRequest request) {
+        verifyRegisteredMember(request.memberId());
+
+        LocalDate startDate = request.getStartDate();
+        LocalDate endDate = request.getEndDate();
+
+        List<WorkHoursInquiryResponse> workHoursInquiryResponses = findWorkHoursInquiry(request, startDate, endDate);
+        long totalWorkMinute = calculateWorkMinutes(workHoursInquiryResponses);
+
+        return WorkHoursInquiryResponses.of(workHoursInquiryResponses, totalWorkMinute);
+    }
+
     private void verifyTodayClockIn(Long memberId, LocalDateTime clockInDateTime) {
         LocalDate startDate = LocalDateUtil.convertToDateOneDaysAgo(clockInDateTime);
         LocalDate endDate = LocalDateUtil.convertToDateOneDayLater(clockInDateTime);
@@ -67,5 +82,33 @@ public class AttendanceService {
         if (attendanceRepository.existsByMemberIdAndDateRange(memberId, startDate, endDate)) {
             throw new VerificationException(ALREADY_CLOCKED_IN);
         }
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(UNREGISTERED_MEMBER));
+    }
+
+    private void verifyRegisteredMember(Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new VerificationException(UNREGISTERED_MEMBER);
+        }
+    }
+
+    private List<WorkHoursInquiryResponse> findWorkHoursInquiry(WorkHoursInquiryRequest request, LocalDate startDate, LocalDate endDate) {
+        return attendanceRepository
+                .findByMemberIdAndEndDateNotNullAndDateRange(request.memberId(), startDate, endDate)
+                .stream()
+                .map(attendance -> {
+                    LocalDateTime clockInDate = attendance.clockInDateTime();
+                    Long workHour = attendance.calculateWorkHour();
+                    return WorkHoursInquiryResponse.of(clockInDate, workHour);
+                }).toList();
+    }
+
+    private long calculateWorkMinutes(List<WorkHoursInquiryResponse> workHoursInquiryResponses) {
+        return workHoursInquiryResponses.stream()
+                .mapToLong(WorkHoursInquiryResponse::workingMinutes)
+                .sum();
     }
 }
