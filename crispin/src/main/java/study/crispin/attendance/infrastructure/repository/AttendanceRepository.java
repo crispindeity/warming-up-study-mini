136072886c004fbe5dc2b31efd56c15dd049faf2
crispin <shoy1415@gmail.com>
2024-03-06 22:01:02 +0900

    :sparkles: Feat: add findByMemberIdAndEndDateNotNullAndDateRange method to the attendance repository
      - findByMemberIdAndEndDateNotNullAndDateRange 메서드 추가

--- crispin/src/main/java/study/crispin/attendance/infrastructure/repository/AttendanceRepository.java
+++ crispin/src/main/java/study/crispin/attendance/infrastructure/repository/AttendanceRepository.java
@@ -3,12 +3,15 @@ package study.crispin.attendance.infrastructure.repository;
 import study.crispin.attendance.domain.Attendance;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceRepository {
     Attendance save(Attendance attendance);
 
     Optional<Attendance> findByMemberIdAndDateRange(Long memberId, LocalDate startDate, LocalDate endDate);
+
+    List<Attendance> findByMemberIdAndEndDateNotNullAndDateRange(Long memberId, LocalDate startDate, LocalDate endDate);
 
     boolean existsByMemberIdAndDateRange(Long memberId, LocalDate startDate, LocalDate endDate);
 }
