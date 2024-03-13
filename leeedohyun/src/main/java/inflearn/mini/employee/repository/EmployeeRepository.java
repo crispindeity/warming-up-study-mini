1a152675cbc3182aa3708c23b2a077eb5936807d
leeedohyun <ldh19738@gmail.com>
2024-03-13 13:17:00 +0900

    refactor: 불필요한 메서드 제거

--- leeedohyun/src/main/java/inflearn/mini/employee/repository/EmployeeRepository.java
+++ leeedohyun/src/main/java/inflearn/mini/employee/repository/EmployeeRepository.java
@@ -1,12 +1,8 @@
 package inflearn.mini.employee.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import inflearn.mini.employee.domain.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-    Optional<Employee> findById(final Long id);
 }
